3dfb1063018c31b028bb93be66d51edddee626b5
Anant Raj <anant9304@gmail.com>
2025-11-07 12:22:10 +0530

    Add files via upload

---
+++ lab-task-44.c
@@ -0,0 +1,10 @@
+//WAP to print the numbers between 1 to 100 using while loop.
+#include <stdio.h>
+int main() {
+    int i = 1;
+    while(i <= 100) {
+        printf("%d ", i);
+        i++;
+    }
+    return 0;
+}
---
+++ lab-task-45.c
@@ -0,0 +1,10 @@
+//WAP to print the numbers between 1 to 100 using do while loop.
+#include <stdio.h>
+int main() {
+    int i = 1;
+    do {
+        printf("%d ", i);
+        i++;
+    } while(i <= 100);
+    return 0;
+}
